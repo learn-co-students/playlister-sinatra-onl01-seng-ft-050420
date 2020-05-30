d97d38178b1838a1bd545bbf18844c28f05fd1a2
github username <github email address>
2020-05-29 22:19:35 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -32,7 +32,22 @@ class SongsController < ApplicationController
 
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
+    @genres = Genre.all
+    erb :"/songs/edit"
+  end 
 
+  patch '/songs/:slug' do 
+    song = Song.find_by_slug(params[:slug])
+    song.update(params[:song])
+    song.genres = Genre.find(params[:genres])
+
+    unless params[:song][:name].empty?
+      song.artist = Artist.find_or_create_by(params[:artist])
+      song.save
+    end 
+
+    flash[:message] = "Successfully updated song."
+    redirect to "/songs/#{song.slug}"
   end 
 
 end
