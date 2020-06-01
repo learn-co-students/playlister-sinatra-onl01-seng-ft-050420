d764ac2c73d2a11c742d3b77dc3dd2a37cb73351
Luke Chen <luke.chen.2010@gmail.com>
2020-06-01 23:14:08 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,6 +22,16 @@ class ApplicationController < Sinatra::Base
     erb :'/songs/new'
   end
   
+  post '/songs' do
+    @song = Song.create(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.genre_ids = params[:genres]
+    @song.save
+
+    flash[:message] = "Successfully created song."
+    redirect("/songs/#{@song.slug}")
+  end
+  
   #move to artists
   get '/artists' do
     erb :'/artists/index'
