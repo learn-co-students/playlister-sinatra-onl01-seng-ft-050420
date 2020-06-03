e41ad854cb51acade407b3afef1c94afaa01361f
Luke Chen <luke.chen.2010@gmail.com>
2020-06-03 22:20:06 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
   use Rack::Flash
-   
+  
+  enable :sessions
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
